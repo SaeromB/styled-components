fb3b34d8de9b4e0a35f8c0da265b65689bb7b642
Saerom <saerombang11@gmail.com>
2019-06-17 13:31:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+const Heading = styled.h1`
+  font-size: 2rem;
+
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Heading>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Heading>
+        <Heading>
+          Heading2
+        </Heading>
         <a
           className="App-link"
           href="https://reactjs.org"
